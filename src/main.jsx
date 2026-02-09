7fef245dcd6f46a55d35b92e9b228b070aaa6db7
desafioviolao10em30-dotcom <desafioviolao10em30@gmail.com>
2026-02-09 15:06:40 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import Ebook from './paginas/desafio/Ebook.jsx'
+import Ebook from './pages/desafio/Ebook.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
