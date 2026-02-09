9d5cc012252f343b0941347ef317e60336021e52
desafioviolao10em30-dotcom <desafioviolao10em30@gmail.com>
2026-02-09 11:18:41 -0300

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "
+App_Desafio_violao_10_em_30", // 
+});
