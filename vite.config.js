572d7d61b693b80efac68fb40b4e3706fd002fec
desafioviolao10em30-dotcom <desafioviolao10em30@gmail.com>
2026-02-09 16:49:51 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/App_Desafio_violao_10_em_30/',
-})
+  base: "/App_Desafio_violao_10_em_30/",
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
