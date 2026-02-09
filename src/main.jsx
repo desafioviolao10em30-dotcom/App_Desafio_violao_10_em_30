d06180b42a239c0db6050d5875a4e6b57ca11950
desafioviolao10em30-dotcom <desafioviolao10em30@gmail.com>
2026-02-09 18:04:16 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import Ebook from './pages/desafio/Ebook.jsx'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Ebook />
     </BrowserRouter>
   </React.StrictMode>
