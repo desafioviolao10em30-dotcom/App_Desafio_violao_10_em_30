f3e306d680428007b8427a52b16377e69cff0cbe
desafioviolao10em30-dotcom <desafioviolao10em30@gmail.com>
2026-02-10 05:41:08 -0300

    Update obrigado.js

--- pages/obrigado.js
+++ pages/obrigado.js
@@ -1,1 +1,9 @@
+registerRoute('/obrigado', () => `
+  <div class="card">
+    <h2>Inscrição Confirmada ✅</h2>
+    <p>
+      Agora siga as instruções que enviamos no WhatsApp.
+    </p>
+  </div>
+`);
 
