71857708f65c2e14b0b6155b51177fb3e71f1b54
desafioviolao10em30-dotcom <desafioviolao10em30@gmail.com>
2026-02-10 05:38:29 -0300

    Update router.js

--- router.js
+++ router.js
@@ -1,1 +1,28 @@
+const routes = {};
+
+function registerRoute(path, renderFn) {
+  routes[path] = renderFn;
+}
+
+function router() {
+  const hash = location.hash || '#/home';
+  const route = hash.replace('#', '');
+
+  const view = document.getElementById('view');
+  view.innerHTML = '';
+
+  if (routes[route]) {
+    view.innerHTML = routes[route]();
+  } else {
+    view.innerHTML = `
+      <div class="card">
+        <h2>Página não encontrada</h2>
+        <a href="#/home" class="button">Voltar</a>
+      </div>
+    `;
+  }
+}
+
+window.addEventListener('hashchange', router);
+window.addEventListener('load', router);
 
