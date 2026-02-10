b85703375765b26ad7d468cba19104cabf777e09
desafioviolao10em30-dotcom <desafioviolao10em30-dotcom@users.noreply.github.com>
2026-02-10 10:23:36 +0000

    deploy: 844eb447219a35552836fe594ffdffb89687c16d

---
+++ assets/obrigado-Li_8s7mm.js
@@ -0,0 +1,8 @@
+registerRoute("/obrigado",()=>`
+  <div class="card">
+    <h2>Inscrição Confirmada ✅</h2>
+    <p>
+      Agora siga as instruções que enviamos no WhatsApp.
+    </p>
+  </div>
+`);
