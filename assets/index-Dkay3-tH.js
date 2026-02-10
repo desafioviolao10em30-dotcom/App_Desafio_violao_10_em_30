d85d1acd229673fcd47764732ac072fafa788510
desafioviolao10em30-dotcom <desafioviolao10em30-dotcom@users.noreply.github.com>
2026-02-10 16:28:33 +0000

    deploy: 1a704412770924072c9a85619fc16381689a8290

---
+++ assets/index-Dkay3-tH.js
@@ -0,0 +1,5 @@
+(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&o(s)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();const c={};async function a(){const n=document.getElementById("view"),i=(window.location.hash||"#/home").replace("#",""),o=c[i]||c["/home"];try{const e=o();n.innerHTML=e instanceof Promise?await e:e}catch(e){console.error(e),n.innerHTML=`
+      <div class="card">
+        <h2>Erro ao carregar página</h2>
+      </div>
+    `}}function d(){window.addEventListener("hashchange",a),a()}window.addEventListener("DOMContentLoaded",()=>{d()});
--- assets/obrigado-Li_8s7mm.js
+++
@@ -1,8 +0,0 @@
-registerRoute("/obrigado",()=>`
-  <div class="card">
-    <h2>Inscrição Confirmada ✅</h2>
-    <p>
-      Agora siga as instruções que enviamos no WhatsApp.
-    </p>
-  </div>
-`);
