e54528e3d8c606f447eec195360aa077bd6654f8
desafioviolao10em30-dotcom <desafioviolao10em30@gmail.com>
2026-02-10 05:40:05 -0300

    Update ebook.js

--- pages/ebook.js
+++ pages/ebook.js
@@ -1,1 +1,9 @@
+registerRoute('/ebook', () => `
+  <div class="card">
+    <h2>Ebook Interativo</h2>
+    <p>
+      Aqui entra toda a estrutura do ebook que você já validou.
+    </p>
+  </div>
+`);
 
