f05e941582f75eec7972cd470acdd56598e24be1
desafioviolao10em30-dotcom <desafioviolao10em30@gmail.com>
2026-02-10 13:28:38 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,7 @@
-// supabase.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const SUPABASE_URL = 'https://npxgzneiemntoedhdbca.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5weGd6bmVpZW1udG9lZGhkYmNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3MDI4NjYsImV4cCI6MjA4NjI3ODg2Nn0.10hOtJVelqlbhrQhiq6Ua27dA5ARlRjDrUlGYyegIns';
+const SUPABASE_ANON_KEY = 'SUA_ANON_PUBLIC_KEY_AQUI';
 
 export const supabase = createClient(
   SUPABASE_URL,
