bebe73710dd9737d04b92fd6fb94d763213012aa
desafioviolao10em30-dotcom <desafioviolao10em30@gmail.com>
2026-02-10 05:39:41 -0300

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -1,1 +1,10 @@
+registerRoute('/home', () => `
+  <div class="card">
+    <h1>Desafio Violão 10 em 30 🎸</h1>
+    <p>
+      Do zero às suas primeiras 10 músicas em apenas 30 dias.
+    </p>
+    <a href="#/ebook" class="button">Acessar Ebook</a>
+  </div>
+`);
 
